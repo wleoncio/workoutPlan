9b36e850bfdf8fdd522d2f907d3c78bc0312c066
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-18 07:13:48 +0100

    Added logging (closes #3)

---
+++ logExercise.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+class Exercise:
+  def __init__(self, url, folder="log"):
+    base_url = "https://darebee.com/workouts/"
+    suffix = "-workout.html"
+    self.__file_html_name = url.replace(base_url, "")
+    self.exercise_name = self.__file_html_name.replace(suffix, "")
+    self.file_name = folder + "/" + self.exercise_name + ".csv"
+    self.read()
+    to_save = input("Save new workout [y/N]?: ")
+    if to_save == "y":
+      self.save()
+
+  def __str__(self):
+    print(self.exercise_name)
+
+  def save(self):
+    # Gathering data
+    self.date = input("Enter date: ")
+    self.level = input("Enter level: ")
+    self.pause = input("Enter pause: ")
+    self.weight = input("Enter weight: ")
+
+    # Saving data
+    stream = open(self.file_name, "at")
+    line = self.date + "," + self.level + "," + self.pause + "," + self.weight + "\n"
+    stream.write(line)
+    stream.close()
+
+  def read(self):
+    stream = open(self.file_name, "rt")
+    print("Date\t\tLevel\tPause\tWeights")
+    for line in stream:
+      line_split = line.split(",")
+      line_string = '\t'.join(line_split).replace("\n","")
+      print(line_string)
+    stream.close()
+
+# Test
+x = Exercise("https://darebee.com/workouts/gravity-workout.html")
