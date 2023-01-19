0aa8538e2533aa2beb66605af7783cc7b46ef993
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-19 21:17:53 +0100

    Added streak counting

--- 4-finish.py
+++ 4-finish.py
@@ -13,11 +13,40 @@ if not exists(path + file):
 # Reading content
 stream = open(path + file, "rt")
 print("Date\t\tUpper\tAbs\tLower\tCardio")
+streak_upper = []
+streak_abs = []
+streak_lower = []
+streak_cardio = []
 for line in stream: # TODO: DRY (same code in Exercise class)
     line_split = line.split(",")
+    streak_upper.append(line_split[1] == 'True')
+    streak_abs.append(line_split[2] == 'True')
+    streak_lower.append(line_split[3] == 'True')
+    streak_cardio.append(line_split[4] == 'True\n')
     line_string = '\t'.join(line_split).replace("\n","")
     print(line_string)
 stream.close()
+
+def calcStreak(x):
+    x.reverse()
+    done_streak = 0
+    for element in x:
+        if element:
+            done_streak += 1
+        if not element:
+            break
+    rest_streak = 0
+    for element in x:
+        if not element:
+            rest_streak +=1
+        else:
+            break
+    return([done_streak, rest_streak])
+
+print('Upper streak  [done, rest]: ' + str(calcStreak(streak_upper)))
+print('Abs streak    [done, rest]: ' + str(calcStreak(streak_abs)))
+print('Lower streak  [done, rest]: ' + str(calcStreak(streak_lower)))
+print('Cardio streak [done, rest]: ' + str(calcStreak(streak_cardio)))
 
 # Reading new content
 today = datetime.datetime.now().strftime("%Y-%m-%d")
