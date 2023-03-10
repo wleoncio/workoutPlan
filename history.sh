75d799b0ecd710262f4c3268662ee030b6950028
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-03-10 09:44:52 +0100

    Added alfabetical ordering of history

--- history.sh
+++ history.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-# Prints the exercise catalog by date
+# Prints the exercise catalog by date. If an "abc" argument is passed, orders alfabetically.
 
-ls -oglt --time-style=long-iso log --hide="day.csv"
+if [[ $1 = "abc" ]]
+then
+	echo "Sorting alfabetically"
+	t=""
+else
+	echo "Sorting by time"
+	t="t"
+fi
+
+ls -ogl$t --time-style=long-iso log --hide="day.csv"
